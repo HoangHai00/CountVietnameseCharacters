e7d31a4c6554b8bdebbec123bc88d7a5dacabb2f
HoangHai00 <hai.k64cntt.bkhn@gmail.com>
2024-02-20 23:41:00 +0700

    update Count Vietnamese Characters

--- CountVietnameseCharacters/app/src/main/java/com/example/countvietnamesecharacters/MainActivity.kt
+++ CountVietnameseCharacters/app/src/main/java/com/example/countvietnamesecharacters/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.countvietnamesecharacters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.countvietnamesecharacters.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+interface ClickHandler {
+    fun onClick()
+}
+
+class MainActivity : AppCompatActivity(), ClickHandler {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = MainViewModel()
+        binding.viewModel = viewModel
+        binding.clickHandler = this
+        binding.lifecycleOwner = this
+    }
+
+    override fun onClick() {
+
+        viewModel.isButtonClicked.value = true
+        viewModel.countVietnameseCharacters(binding.editText.text.toString())
+
     }
 }
